b59420d0220e594e5b19b9dac6d2184039b38737
Katie Pearson <katelin.d.pearson24@gmail.com>
2024-11-21 15:00:49 -0800

    Add SSH logo to footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,9 @@
 <footer>
-	<!--
 	<div class="logo-gallery">
+		<a href="https://symbiota.org/" target="_blank" title="<?= "SSH Logo" ?>" aria-label="Visit Symbiota website">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/SSH.png"  alt="SSH Logo" />
+		</a>
+		<!--
 		<?php
 		//include($SERVER_ROOT . '/accessibility/module.php');
 		?>
@@ -10,11 +13,9 @@
 		<a href="http://idigbio.org" target="_blank" title="iDigBio" aria-label="<?= $LANG['F_VISIT_IDIGBIO'] ?>">
 			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_idig.png" alt="<?= $LANG['F_IDIGBIO_LOGO'] ?>" />
 		</a>
-		<a href="https://biokic.asu.edu" target="_blank" title="<?= $LANG['F_BIOKIC'] ?>" aria-label="Visit BioKIC website">
-			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png"  alt="<?= $LANG['F_BIOKIC_LOGO'] ?>" />
 		</a>
+		-->
 	</div>
--->
 	<p>
 		<?= $LANG['F_POWERED_BY'] ?> <a href="https://symbiota.org/" target="_blank">Symbiota</a>.
 	</p>
