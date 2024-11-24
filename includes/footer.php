32a2a98b2c48609112fe7401febb8933cf58bbd8
Katie Pearson <katelin.d.pearson24@gmail.com>
2024-11-23 19:39:42 -0800

    Update footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,18 +1,26 @@
-    	</td>
-	</tr>
-	<tr>
-		<td id="footer" colspan="3">
-			<div style="width:650px;height:100px;margin-left:auto;margin-right:auto;margin-bottom:15px;display:table;" >
-				<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/NSF_web_transperent.gif" width="100" height="100" alt="The U.S. National Science Foundation" /> 
-				<div style="padding-left:10px;display:table-cell;vertical-align: middle;">
-					Development supported by U.S. National Science Foundation Grants 
+<footer>
+	<div class="logo-gallery">
+		<?php
+		//include($SERVER_ROOT . '/accessibility/module.php');
+		?>
+		<a href="https://www.nsf.gov" target="_blank" aria-label="<?= $LANG['F_VISIT_NSF'] ?>">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" alt="<?= $LANG['F_NSF_LOGO'] ?>" />
+		</a>
+		<a href="http://idigbio.org" target="_blank" title="iDigBio" aria-label="<?= $LANG['F_VISIT_IDIGBIO'] ?>">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_idig.png" alt="<?= $LANG['F_IDIGBIO_LOGO'] ?>" />
+		</a>
+		<a href="https://biokic.asu.edu" target="_blank" title="<?= $LANG['F_BIOKIC'] ?>" aria-label="Visit BioKIC website">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png"  alt="<?= $LANG['F_BIOKIC_LOGO'] ?>" />
+		</a>
+	</div>
+	<p>
+	Development supported by U.S. National Science Foundation Grants 
 					(<a href="http://nsf.gov/awardsearch/showAward.do?AwardNumber=9983132" target="_blank">DBI 9983132</a>, 
 					<a href="http://nsf.gov/awardsearch/showAward.do?AwardNumber=0237418" target="_blank">BRC 0237418</a>, 
 					<a href="http://www.nsf.gov/awardsearch/showAward.do?AwardNumber=0743827" target="_blank">DBI 0743827</a>,
 					<a href="http://www.nsf.gov/awardsearch/showAward.do?AwardNumber=0847966" target="_blank">DBI 0847966</a>)
-				<br>Powered by <a href="https://symbiota.org/" target="_blank">Symbiota</a>
-				</div>
-			</div>
-		</td>
-	</tr>
-</table>
+	</p>
+	<p>
+		<?= $LANG['F_POWERED_BY'] ?> <a href="https://symbiota.org/" target="_blank">Symbiota</a>.
+	</p>
+</footer>
