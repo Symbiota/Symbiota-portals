32b2078bced90592ed16aab60a34ff400f44338d
Gregory Post <gregory.post@ku.edu>
2024-11-26 09:45:27 -0700

    soro3.0.31 initial sync from production server

--- includes/footer.php
+++ includes/footer.php
@@ -5,13 +5,13 @@
 			<div style="width:80%;margin-left:auto;margin-right:auto;margin-bottom:15px;margin-top:15px;display:table;" >
 				<div style="width:100px;padding:0px 20px;display:table-cell;vertical-align: middle;">
 					<a href="https://www.nsf.gov/awardsearch/showAward.do?AwardNumber=1702516"target="_blank"> 
-						<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/NSF_web_transperent.gif" width="100" height="100" alt="The National Science Foundation" /> 
+						<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/NSF_web_transperent.gif" width="100" height="100" alt="The U.S. National Science Foundation" /> 
 					</a>
 				</div>
 				<div style="padding-left:10px;display:table-cell;vertical-align: middle;">
 					<b>This project made possible by 
 						<a href="https://www.nsf.gov/awardsearch/showAward.do?AwardNumber=1702516" target="_blank">
-							National Science Foundation Award EF 1702516
+							U.S. National Science Foundation Award EF 1702516
 						</a>
 					</b>
 					<br>
