e80a5ee53048c42245fefe3a21ed416a0d1dcc0e
Katie Pearson <katelin.d.pearson24@gmail.com>
2024-12-05 10:53:30 -0800

    Add KU logo, change homepage

--- index.php
+++ index.php
@@ -80,9 +80,26 @@ header('Content-Type: text/html; charset=' . $CHARSET);
 		</div>
             <h1 style="">Welcome to TORCH Data Portal</h1>
  		<p>
-			<p>The Texas Oklahoma Regional Consortium of Herbaria (TORCH) was developed to advocate for and to organize approximately 4 million plant specimens across more than 50 herbaria in the two-state region. Learn more about TORCH and its members at <a style="text-decoration: underline;" href="http://www.torcherbaria.org/" target="_blank">torcherbaria.org</a>.</p>
-			<p>The TORCH data portal provides access to specimen data and associated images from our herbaria to facilitate botanical research for the purpose of conservation, management, and education. This is an open access portal powered by Symbiota (<a style="text-decoration: underline;" href="http://www.symbiota.org/" target="_blank">symbiota.org</a>). Our data records are aggregated by iDigBio (<a style="text-decoration: underline;" href="http://www.idigbio.org/" target="_blank">idigbio.org</a>; the National Resource for Advancing Digitization of Biodiversity Collections, funded by the National Science Foundation). New records are made available as specimens are digitized (imaged, databased, and georeferenced) by participating herbaria. If you are interested in assisting with digitization efforts, please contact the appropriate curator or collections manager.</p>
-			<p>To learn more about the features and capabilities of the Symbiota software used by this portal, visit the <a style="text-decoration: underline;" href="http://symbiota.org/docs/" target="_blank">Symbiota Help Pages</a>.</p>
+			<p>
+				The Texas Oklahoma Regional Consortium of Herbaria (TORCH) was developed to advocate for and to organize approximately 4 million plant specimens 
+				across more than 50 herbaria in the two-state region. Learn more about TORCH and its members at 
+				<a style="text-decoration: underline;" href="http://www.torcherbaria.org/" target="_blank">torcherbaria.org</a>.
+			</p>
+			<p>
+				The TORCH data portal provides access to specimen data and associated images from our herbaria to facilitate botanical research for 
+				the purpose of conservation, management, and education. This is an open access portal powered by Symbiota 
+				(<a style="text-decoration: underline;" href="http://www.symbiota.org/" target="_blank">symbiota.org</a>). Our data records are aggregated by 
+				iDigBio (<a style="text-decoration: underline;" href="http://www.idigbio.org/" target="_blank">idigbio.org</a>; the National Resource for Advancing 
+				Digitization of Biodiversity Collections, funded by the National Science Foundation). New records are made available as specimens are 
+				digitized (imaged, databased, and georeferenced) by participating herbaria. If you are interested in assisting with digitization efforts, 
+				please contact the appropriate curator or collections manager.
+			</p>
+			<p>
+				This site is brought to you in collaboration with the <a href="http://symbiota.org/seinet/" target="_blank">SEINet Network</a>, which contains 24 million records from over 450 collections. 
+				Collections are organized into regional consortia that are accessed through different websites but share a central database. To learn more 
+				about the features and capabilities of the Symbiota software used by this portal, visit the 
+				<a style="text-decoration: underline;" href="http://symbiota.org/docs/" target="_blank">Symbiota Help Pages</a>.
+			</p>
 		</p>
 	</main>
 
