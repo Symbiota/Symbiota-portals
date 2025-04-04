4fab5acb802de27e9d83127cc32f221b23509f64
Katie Pearson <katelin.d.pearson24@gmail.com>
2025-04-04 06:06:01 -0700

    Add KUBI logo

--- includes/footer.php
+++ includes/footer.php
@@ -3,8 +3,11 @@
 		<?php
 		//include($SERVER_ROOT . '/accessibility/module.php');
 		?>
-		<a href="https://biokic.asu.edu" target="_blank" title="<?= $LANG['F_BIOKIC'] ?>" aria-label="Visit BioKIC website">
+		<a href="https://biodiversity.ku.edu/" target="_blank" title="<?= $LANG['F_BIOKIC'] ?>" aria-label="Visit BioKIC website">
 			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png"  alt="<?= $LANG['F_BIOKIC_LOGO'] ?>" />
+		</a>
+		<a href="https://biokic.asu.edu" target="_blank" title="KU BI" aria-label="Visit KU BI website">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/KUBI_bw.png"  alt="KU BI" />
 		</a>
 	</div>
 	<p>
