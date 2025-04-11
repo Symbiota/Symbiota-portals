4e4eebcd199ab7e55048eac3c21e28ccc7267554
Katie Pearson <katelin.d.pearson24@gmail.com>
2025-04-10 20:10:07 -0700

    Add language selector to header

--- includes/header.php
+++ includes/header.php
@@ -113,16 +113,13 @@ $collectionSearchPage = $SHOULD_USE_HARVESTPARAMS ? '/collections/index.php' : '
 							<?= $LANG['H_SITEMAP'] ?>
 						</a>
 					</li>
-					<!--
 					<li id="lang-select-li">
 						<label for="language-selection"><?= $LANG['H_SELECT_LANGUAGE'] ?>: </label>
 						<select oninput="setLanguage(this)" id="language-selection" name="language-selection">
 							<option value="en">English</option>
 							<option value="es" <?= ($LANG_TAG=='es'?'SELECTED':'') ?>>Español</option>
-							<option value="fr" <?= ($LANG_TAG=='fr'?'SELECTED':'') ?>>Français</option>
 						</select>
 					</li>
-					-->
 				</ul>
 			</nav>
 		</div>
