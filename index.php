57c585e14b47c35dfd5bc0a3620e8e19d240b0d6
Katie Pearson <katelin.d.pearson24@gmail.com>
2025-12-02 06:45:36 -0800

    Add donation button & logo

--- index.php
+++ index.php
@@ -74,6 +74,9 @@ header('Content-Type: text/html; charset=' . $CHARSET);
 			Please send questions or comments to <a href="mailto:Kristian.Valles@usu.edu">Kristian.Valles@usu.edu</a>. 
 			</p>
 		</main>
+	<?php if($GLOBALS['DONATE_LINK'] && file_exists($SERVER_ROOT . '/includes/donationButton.php')): ?>
+		<?php include($SERVER_ROOT . '/includes/donationButton.php') ?>
+	<?php endif ?>
 	<?php
 	include($SERVER_ROOT."/includes/footer.php");
 	?> 
